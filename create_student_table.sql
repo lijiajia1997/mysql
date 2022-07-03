05e7b2b1c0d121e8f19ca1310d5dbee5008a0795
Jiajia <30934277+lijiajia1997@users.noreply.github.com>
2022-07-03 18:39:24 +1000

    Update create_student_table.sql

--- create_student_table.sql
+++ create_student_table.sql
@@ -1,7 +1,7 @@
 DROP TABLE student;
 
 CREATE TABLE student (
-    student_id INT AUTO_INCREMENT,
+    student_id INT,
     name VARCHAR(20),
     major VARCHAR(20),
     PRIMARY KEY(student_id)
@@ -10,11 +10,11 @@ CREATE TABLE student (
 --DESCRIBE student;
 SELECT * FROM student; 
 
-INSERT INTO student(name, major) VALUES('Jack', 'Biology');
-INSERT INTO student(name, major) VALUES('Kate', 'Sociology');
-INSERT INTO student(name, major) VALUES('Claire', 'Chemistry');
-INSERT INTO student(name, major) VALUES('Jack', 'Biology');
-INSERT INTO student(name, major) VALUES('Mike', 'Computer Science');
+INSERT INTO student VALUES(1, 'Jack', 'Biology');
+INSERT INTO student VALUES(2, 'Kate', 'Sociology');
+INSERT INTO student VALUES(3, 'Claire', 'Chemistry');
+INSERT INTO student VALUES(4, 'Jack', 'Biology');
+INSERT INTO student VALUES(5, 'Mike', 'Computer Science');
 
 --ALTER TABLE student ADD gpa DECIMAL(3,2);
 --ALTER TABLE student DROP COLUMN gpa;
